daf3c9e28e5b05b4722ac0642bf27af1fe4a1637
Leuc117 <43831125+Leuc117@users.noreply.github.com>
2019-04-02 19:21:15 -0400

    Stuff

--- MAINDRONECODE.m
+++ MAINDRONECODE.m
@@ -7,60 +7,70 @@ clear;
 
 %this section finds the dimensions and physical properties of various parts
 %of the drone
-wingTheo = input("Theoretical area of wing: ");
-wingSpan = input("Wing Span: ");
-wingThick = input("Wing Thickness: ");
+drone = input('prototype or medical: ', 's');
+wingTheo = input('Theoretical area of wing: ');
+wingSpan = input('Wing Span: ');
 
-wingWet = input("Wetted area of wing: ");
-fuselWet = input("Wetted area of fuselage: ");
-tailWet = input("Wetted area of tail: ");
+wingWet = input('Wetted area of wing: ');
+fuselWet = input('Wetted area of fuselage: ');
+horTailWet = input('Wetted area of horizontal tail: ');
+vertTailWet = input('Wetted area of vertical tail: ');
 
-fuselDiam = input("Average diameter of fuselage: ");
-fuselLength = input("Overall length of fuselage: ");
+fuselDiam = input('Average diameter of fuselage: ');
+fuselLength = input('Overall length of fuselage: ');
 
-horTailLength = input("Length of the horizontal tail: ");
+avgWingThick = input('Average thickness of the wing: ');
+avgHorTailThick = input('Average thickness of the horizontal tail: ');
+avgVertTailThick = input('Average thickness of the vertical tail: ');
 
-avgWingThick = input("Average thickness of the wing: ");
-avgHorTailThick = input("Average thickness of the horizontal tail: ");
-avgVertTailThick = input("Average thickness of the vertical tail: ");
+avgWingChord = input('Average chord length of wing: ');
+avgHorTailChord = input('Average chord length of horizontal tail: ');
+avgVertTailChord = input('Average chord length of vertical tail: ');
 
-avgWingChord = input("Average chord length of wing: ");
-avgHorTailChord = input("Average chord length of horizontal tail: ");
-avgVertTailChord = input("Average chord length of horizontal tail: ");
+droneMass = input('Mass of the drone: ');
+battMass = input('Mass of the battery: ');
 
-droneMass = input("Mass of the drone: ");
-battMass = input("Mass of the battery: ");
-
-%some constants are defined
-RPM = 1806; %emax 1806
-pitch  =.0762;
-diam = .1524;
-velocity = 1:30;
-drag = 1:30;
-batteryEnergy = .36;
+if (strcmp(drone, 'prototype'))
+    RPM = 15000;
+    pitch  =.0762;
+    diam = .1524;
+    velocity = 1:30;
+    drag = 1:30;
+    batteryEnergy = 360000;
+elseif (strcmp(drone, 'medical'))
+    RPM = 1806;
+    pitch  =.22;
+    diam = .254;
+    velocity = 1:30;
+    drag = 1:30;
+    batteryEnergy = 720000;
+end
 
 %output begins
-fprintf('Overall fuselage length ...... %f m', fuselLength);
-fprintf('Overall fuselage length ...... %f kg', droneWeight);
-fprintf('Wing theoretical area ........ %f m^2', wingTheo);
-fprintf('Wing span .................... %f m', wingSpan);
-fprintf('Diameter of fuselage ......... %f m', fuselDiam);
+fprintf('Overall fuselage length ...... %.4f m\n', fuselLength);
+fprintf('Overall drone weight ...... %.4f kg\n', droneMass);
+fprintf('Wing theoretical area ........ %.4f m^2\n', wingTheo);
+fprintf('Wing span .................... %.4f m\n', wingSpan);
+fprintf('Diameter of fuselage ......... %.4f m\n', fuselDiam);
 
 Thrust = thrustFinder(RPM, pitch, diam);
-[drag1, drag2] = dragCoeff(avgWingChord, wingSpan, avgWingThick, avgVertTailChord, ...
-    avgVertTailThick, avgHorTailChord, horTailLength, avgHorTailThick, fuselLength, fuselDiam);
+[drag1, drag2] = dragCoeff(avgWingChord, wingSpan, avgWingThick, wingWet,...
+    avgVertTailChord, avgVertTailThick, vertTailWet, ...
+    avgHorTailChord, avgHorTailThick, horTailWet,...
+    fuselLength, fuselDiam, fuselWet, droneMass + battMass);
 
 for i = 1 : 30
-   drag(i) = drag1 * i^2 + (c2/(i^2)); 
+   drag(i) = (drag2 * i^2) + (drag1/(i^2)); 
 end
 
 plot(Thrust, velocity, 'g');
 hold on;
 plot(drag, velocity, 'r');
 
-xlabel("Velocity m/s");
-ylabel("Thrust(green), Drag(red)");
-title("Thrust and Drag as a Function of Velocity");
+xlabel('Velocity m/s');
+ylabel('Thrust(green), Drag(red)');
+title('Thrust and Drag as a Function of Velocity');
 
+%this needs to be done at max velocity
 myRange = range(batteryEnergy, battMass, droneMass, Thrust(30), drag(30));
-fprintf("Range of the Drone.............. %f", myRange);
+fprintf('Range of the Drone.............. %f\n', myRange);
--- dragCoeff.m
+++ dragCoeff.m
@@ -1,16 +1,15 @@
 function [dragCoeff1, dragCoeff2] = dragCoeff(avgChordWing, ...
-    lengthWing, thickWing, avgChordVertTail,  ...
-    thickVertTail, avgChordHorTail,  ...
-    lengthHorTail, thickHorTail, lengthFuse, diamFuse)
+    wingSpan, thickWing, wetWing, avgChordVertTail,  ...
+    thickVertTail, wetVertTail, avgChordHorTail,  ...
+    thickHorTail, wetHorTail, lengthFuse, diamFuse, wetFuse, massDrone)
 %calculates drag coefficients 
 
-%dragCoeff1         coefficient for drag that will be multiplied by v^2
-%dragCoeff2         coefficient for drag that will be multiplied by 1/v^2
+%dragCoeff2         coefficient for drag that will be multiplied by v^2
+%dragCoeff1         coefficient for drag that will be multiplied by 1/v^2
 
-%largeChordxxx      chord at base of wing
-%smallChordxxx      chord at tip of wing
-%lengthxxx          distance from base to tip
-%thickxxx           depth of wing
+%avgChordxxx        average chord of wing
+%wetxxx             wetted area of part
+%thickxxx           average depth of wing
 %lengthFuse         distance on fuselage from tip to tail
 %diamFuse           width of fuselage sideways while looking down at drone
 
@@ -20,43 +19,35 @@ coeffFricWings = (0.455) / (4.556 ^ 2.58);
 coeffFricFuse = (0.455) / (5.26 ^ 2.58);
 
 %main wings
-theoAreaWing = 2 * lengthWing * avgChordWing;
-actAreaWing = theoAreaWing - (avgChordWing * diamFuse);
+sRef = wetWing + (avgChordWing * diamFuse);
+actAreaWing = sRef - (avgChordWing * diamFuse);
 ffWing = 1 + 2 * (thickWing / avgChordWing) + 60 * ...
     ((thickWing / avgChordWing) ^ 4);
-zeroLDragWing = (coeffFricWings * ffWing * actAreaWing) / theoAreaWing;
+zeroLDragWing = (coeffFricWings * ffWing * actAreaWing) / sRef;
 
 %horizontal tail
-theoAreaHorTail = 2 * lengthHorTail * avgChordHorTail;
-actAreaHorTail = theoAreaHorTail - (largeChordHorTail * diamFuse);
 ffHorTail = 1 + 2 * (thickHorTail / avgChordHorTail) + 60 * ...
     ((thickHorTail / avgChordHorTail) ^ 4);
-zeroLDragHorTail = (coeffFricWings * ffHorTail * ...
-    actAreaHorTail) / theoAreaHorTail;
+zeroLDragHorTail = (coeffFricWings * ffHorTail) / sRef;
 
 %vertical tail
 ffVertTail = 1 + 2 * (thickVertTail / avgChordVertTail) + 60 * ...
     ((thickVertTail / avgChordVertTail) ^ 4);
-zeroLDragVertTail = (coeffFricWings * ffVertTail);
+zeroLDragVertTail = (coeffFricWings * ffVertTail) / sRef;
 
 %fuselage
-theoAreaFuse = 2 * pi * (diamFuse / 2) * lengthFuse + ...
-    2 * pi * ((diamFuse / 2) ^ 2);
-actAreaFuse = theoAreaFuse - diamFuse * (largeChordWing + largeChordHorTail);
 ffFuse = 1 + 1.5 * ((diamFuse / lengthFuse) ^ (3 / 2)) + 7 *...
     ((diamFuse / lengthFuse) ^ 3);
-zeroLDragFuse = (coeffFricFuse * ffFuse * actAreaFuse) / theoAreaFuse;
+zeroLDragFuse = (coeffFricFuse * ffFuse * wetFuse) / sRef;
 
 ZLDragCoeff = zeroLDragWing + zeroLDragHorTail + zeroLDragVertTail +...
     zeroLDragFuse;
 
 %% Induced Drag
-aspectRatio = (lengthWing ^ 2) / theoAreaWing;
-liftCoeff = (2 * mass) / (1.134 * theoAreaWing);
+aspectRatio = (wingSpan ^ 2) / sRef;
+liftCoeff = (2 * massDrone * 9.8) / (1.134 * sRef);
 IDragCoeff = (liftCoeff ^ 2) / (pi * 0.85 * aspectRatio);
 
 %% Final drag coefficients
-dragCoeff1 = (1 / 2) * 1.134 * (theoAreaWing + theoAreaHorTail +...
-    theoAreaVertTail + theoAreaFuse) * ZLDragCoeff;
-dragCoeff2 = (1 / 2) * 1.134 * (theoAreaWing + theoAreaHorTail +...
-    theoAreaVertTail + theoAreaFuse) * IDragCoeff;
+dragCoeff1 = (1 / 2) * 1.134 * (sRef) * ZLDragCoeff;
+dragCoeff2 = (1 / 2) * 1.134 * (sRef) * IDragCoeff;
